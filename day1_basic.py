c8064bbcfe8506aa489bed3de2a23e2f10fbed69
michaellc <1753683042@qq.com>
2026-05-05 21:50:05 +0800

    feat: add day1 basic syntax code | 增加第一天基础语法代码

---
+++ day1_basic.py
@@ -0,0 +1,3 @@
+name="michael"
+goal="learn python and find a better job"
+print(f"hello,everyone,I am {name}, my learing goal is: {goal}!")                                                                                     
